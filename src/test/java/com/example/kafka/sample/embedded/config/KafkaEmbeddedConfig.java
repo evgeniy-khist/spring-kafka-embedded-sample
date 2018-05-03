7a383b4392e619f28123e378d9431d620b2c99f9
Evgeniy Khyst <evgeniy.khist@gmail.com>
2018-05-03 22:14:13 +0300

    Add single shared KafkaEmbedded instance as constant to KafkaTestBase

--- src/test/java/com/example/kafka/sample/embedded/config/KafkaEmbeddedConfig.java
+++ src/test/java/com/example/kafka/sample/embedded/config/KafkaEmbeddedConfig.java
@@ -1,10 +1,13 @@
 package com.example.kafka.sample.embedded.config;
 
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
 
-@Configuration
+@PropertySource({"classpath:application.properties", "classpath:test.properties"})
+@EnableConfigurationProperties(KafkaProperties.class)
 public class KafkaEmbeddedConfig {
 
   public static final String TEST_TOPIC = "test_topic";
