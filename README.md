4b488d44580c4e3ab3f1455927a1c59d0eb21225
Evgeniy Khyst <evgeniy.khist@gmail.com>
2018-05-07 20:40:34 +0300

    Parametrize Kafka embedded port

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+This sample demonstrates how to use the same embedded Kafka broker(s) for multiple test classes.
+
 Run this sample using
 
 ```
-./gradlew clean test -i
+./gradlew clean test -i -Dkafka.embedded.port=9092
 ```
--- src/test/java/com/example/kafka/sample/embedded/config/KafkaEmbeddedConfig.java
+++ src/test/java/com/example/kafka/sample/embedded/config/KafkaEmbeddedConfig.java
@@ -1,21 +1,22 @@
 package com.example.kafka.sample.embedded.config;
 
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
 
 @PropertySource({"classpath:application.properties", "classpath:test.properties"})
-@EnableConfigurationProperties(KafkaProperties.class)
 public class KafkaEmbeddedConfig {
 
   public static final String TEST_TOPIC = "test_topic";
 
+  @Value("${kafka.embedded.port:9092}")
+  private int kafkaEmbeddedPort;
+
   @Bean
   public KafkaEmbedded kafkaEmbedded() {
     KafkaEmbedded kafkaEmbedded = new KafkaEmbedded(1, false, 1, TEST_TOPIC);
-    kafkaEmbedded.setKafkaPorts(9092);
+    kafkaEmbedded.setKafkaPorts(kafkaEmbeddedPort);
     return kafkaEmbedded;
   }
 }
